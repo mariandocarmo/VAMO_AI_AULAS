5e225bbfdbcda3c25faf0ea37cbf9ca969e23c6f
mariandocarmo <78182729+mariandocarmo@users.noreply.github.com>
2021-02-22 15:41:30 -0300

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,4 @@
+animal = input("Qual animal você gostaria de ser?")
+print ("Num primeiro momento eu gostaria de ser um", animal)
+segundo_animal = input("Que animal você gostaria de ser no lugar desse?")
+print ("Pensando melhor, eu gostaria de ser uma", segundo_animal)
